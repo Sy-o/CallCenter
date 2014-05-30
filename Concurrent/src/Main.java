71ffe4b77d7221418ea40d58eb417579a925c14d
Sy-o <fieryrobin@gmail.com>
2014-05-30 04:21:19 +0300

    add main

---
+++ Concurrent/src/Main.java
@@ -0,0 +1,22 @@
+import entity.CallCenter;
+import entity.Caller;
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alice
+ * Date: May 30, 2014
+ * Time: 5:08:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        CallCenter center = new CallCenter(3);
+        try {
+            for (int i = 0; i < 12; i++) {
+                Thread.sleep(1);
+                new Caller(center, i).start();
+            }
+        } catch (InterruptedException ex) {
+            System.out.println("Error in Main thread " + Thread.currentThread());
+        }
+    }
+}
